9d4744fdb7b6ce0782770d21f539396da0b8a2ed
Amir Shayegh <shayegh@me.com>
2019-04-04 09:07:51 -0700

    Added ModalView Class + ModalAlert(currently not accessible)

--- Example/Modal/ViewController.swift
+++ Example/Modal/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import Modal
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let test: TestModal = TestModal.nib()
+        test.initialize()
     }
 
     override func didReceiveMemoryWarning() {
